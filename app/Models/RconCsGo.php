b63e24df40e1e199db92d5c3b929d0a9c543487d
Boroboysh <artyomklim001@gmail.com>
2022-08-19 14:43:54 +0300

    Added new Routes; Update RconCsGo.php / web.php.

--- app/Models/RconCsGo.php
+++ app/Models/RconCsGo.php
@@ -4,8 +4,57 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Reflex\Rcon\Exceptions\NotAuthenticatedException;
+use Reflex\Rcon\Exceptions\RconAuthException;
+use Reflex\Rcon\Exceptions\RconConnectException;
+use Reflex\Rcon\Rcon;
 
 class RconCsGo extends Model
 {
     use HasFactory;
+
+    /**
+     * @throws RconConnectException
+     * @throws RconAuthException
+     */
+
+    /**
+     * @throws RconConnectException
+     * @throws RconAuthException
+     * @throws NotAuthenticatedException
+     */
+    public function startMatch($ip, $port, $pass, $filename)
+    {
+        $rcon = new Rcon($ip, $port, $pass);
+        $rcon->connect();
+
+        /*http://109.194.163.83:8000*/
+
+        $rcon->exec('get5_loadmatch_url 109.194.163.83:8000/match_cfg/' .$filename .'.json');
+    }
+
+    /**
+     * @throws RconAuthException
+     * @throws RconConnectException
+     * @throws NotAuthenticatedException
+     */
+    public function endMatch ($ip, $port, $pass) {
+        $rcon = new Rcon($ip, $port, $pass);
+        $rcon->connect();
+
+        $rcon->exec('get5_endmatch');
+    }
+
+    /**
+     * @throws RconConnectException
+     * @throws RconAuthException
+     * @throws NotAuthenticatedException
+     */
+    public function status($ip, $port, $pass)
+    {
+        $rcon = new Rcon($ip, $port, $pass);
+        $rcon->connect();
+
+        return $rcon->exec('get5_status');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,43 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/config/new', function (Request $req){
+Route::get('/configList', function () {
+    $dir = opendir('./../public/match_cfg');
+    $files = [];
+
+    $count = 0;
+
+    while($file = readdir($dir)){
+        if($file == '.' || $file == '..' || is_dir('path/to/dir' . $file)){
+            continue;
+        }
+        $count++;
+        array_push($files, $file);
+    }
+
+    return $files;
+});
+
+Route::get('/configList/{cfg}', function ($filename){
+    return 'Имя файла:' .$filename;
+});
+
+Route::post('/config/new', function (Request $req) {
     $match = $req->all();
+    $server_ip = "217.116.52.85";
+    $server_port = 27115;
+    $server_pass = "89829817111";
 
     $json = json_encode($match);
 
-    $path = public_path('match_cfg/' .$req->input("matchid") .".json");
+    $path = public_path('match_cfg/' . $req->input("matchid") . ".json");
 
-    file_put_contents($req->input("matchid") .".json", $json);
+    file_put_contents($req->input("matchid") . ".json", $json);
 
-    rename($req->input("matchid") .".json", $path);
+    rename($req->input("matchid") . ".json", $path);
+
+    (new App\Models\RconCsGo)->startMatch($server_ip, $server_port, $server_pass, $req->input('matchid'));
+
 
     return response('All okay', 200);
 });
@@ -27,11 +54,27 @@ Route::get('/rcon/status', function () {
     $server_port = 27115;
     $server_pass = "89829817111";
 
-    $rcon =  new Rcon($server_ip, $server_port, $server_pass);
-    $rcon->connect();
+    $res = (new App\Models\RconCsGo)->status($server_ip, $server_port, $server_pass);
 
-    $rcon->exec('bot_add_ct');
-    $response = $rcon->exec('status');
+    return $res;
+});
 
-    return compact('response');
+Route::get('/rcon/start/{filename}', function ($filename) {
+    $server_ip = "217.116.52.85";
+    $server_port = 27115;
+    $server_pass = "89829817111";
+
+    (new App\Models\RconCsGo)->startMatch($server_ip, $server_port, $server_pass, $filename);
+
+    return 'Okay';
+});
+
+Route::get('/rcon/end', function () {
+    $server_ip = "217.116.52.85";
+    $server_port = 27115;
+    $server_pass = "89829817111";
+
+    (new App\Models\RconCsGo)->endMatch($server_ip, $server_port, $server_pass);
+
+    return 'Match ended';
 });
