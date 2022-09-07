4fb549b769d3e7e278f2e6731bc94ba9aa7c6004
Boroboysh <artyomklim001@gmail.com>
2022-09-07 18:35:47 +0300

    Added simple registration

--- database/migrations/2022_09_07_133903_users.php
+++ database/migrations/2022_09_07_133903_users.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,14 +13,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->id('steamid');
+            $table->string('nickname');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar')->nullable();
+            $table->string('currentTeam')->nullable();
             $table->timestamps();
         });
+
     }
 
     /**
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Nette\Schema\Schema;
 use Reflex\Rcon\Rcon;
 
 Route::get('/', function () {
@@ -29,34 +30,25 @@ Route::get('/configList/{cfg}', function ($filename){
     return 'Имя файла:' .$filename;
 });
 
-Route::post('/config/new', function (Request $req) {
-    $match = $req->all();
-    $server_ip = "217.116.52.85";
-    $server_port = 27115;
-    $server_pass = "89829817111";
-
-    $json = json_encode($match);
-
-    $path = public_path('match_cfg/' . $req->input("matchid") . ".json");
-
-    file_put_contents($req->input("matchid") . ".json", $json);
-
-    rename($req->input("matchid") . ".json", $path);
-
-    (new App\Models\RconCsGo)->startMatch($server_ip, $server_port, $server_pass, $req->input('matchid'));
-
-
-    return response('All okay', 200);
-});
-
 Route::get('/rcon/status', function () {
     $server_ip = "217.116.52.85";
     $server_port = 27115;
     $server_pass = "89829817111";
 
-    $res = (new App\Models\RconCsGo)->status($server_ip, $server_port, $server_pass);
+    $response_json = (new App\Models\RconCsGo)->status($server_ip, $server_port, $server_pass);
 
-    return $res;
+    return $response_json;
+});
+
+Route::get('/rcon/currentMatch', function () {
+    $server_ip = "217.116.52.85";
+    $server_port = 27115;
+    $server_pass = "89829817111";
+
+    $status_json = (new App\Models\RconCsGo)->status($server_ip, $server_port, $server_pass);
+    $response =json_decode( preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $status_json), true );;
+
+    return $response['matchid'];
 });
 
 Route::get('/rcon/start/{filename}', function ($filename) {
@@ -77,4 +69,75 @@ Route::get('/rcon/end', function () {
     (new App\Models\RconCsGo)->endMatch($server_ip, $server_port, $server_pass);
 
     return 'Match ended';
+});
+
+Route::post('/config/new', function (Request $req) {
+    $match = $req->all();
+    $server_ip = "217.116.52.85";
+    $server_port = 27115;
+    $server_pass = "89829817111";
+
+    $json = json_encode($match);
+    $path = public_path('match_cfg/' . $req->input("matchid") . ".json");
+
+    file_put_contents($req->input("matchid") . ".json", $json);
+    rename($req->input("matchid") . ".json", $path);
+
+    (new App\Models\RconCsGo)->startMatch($server_ip, $server_port, $server_pass, $req->input('matchid'));
+
+    return response('Ok', 200);
+});
+
+Route::post('/register', function (Request $req) {
+    // steamid, nickname, password, avatar, currentTeam
+    $steamid = $req->input("steamid");
+    $nickname = $req->input("nickname");
+    $password= $req->input("password");
+    $avatar = $req->input("avatar");
+    $currentTeam = $req->input("currentTeam");
+
+    \App\Models\User::create([
+        'steamid' => $steamid,
+        'nickname' => $nickname,
+        'password' => $password,
+        'avatar' => $avatar,
+        'currentTeam' => $currentTeam,
+    ]);
+  /*
+    if (Schema::hasColumn('users', 'steamid')) {
+        return response('User already exists');
+    } else {
+
+    }*/
+
+    return response('User created', 200);
+});
+
+Route::post('/login', function (Request $req) {
+
+});
+
+
+Route::get('/db', function () {
+
+
+    /*создание нового юзера*/
+    $data = \App\Models\User::create([
+        'nickname' => 'One',
+        'password' => '122344',
+        'steamid' => "3414",
+        'currentTeam' => 'Navi',
+        'avatar' => 'url',
+    ]);
+
+    \App\Models\User::create([
+        'nickname' => 'Two',
+        'password' => '122344',
+        'steamid' => "4892",
+        'currentTeam' => 'Navi',
+        'avatar' => 'url',
+    ]);
+
+
+    dd($data);
 });
